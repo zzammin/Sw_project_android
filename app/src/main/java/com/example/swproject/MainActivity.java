59fee85fd2d82739dfebe9196f448f9aec6bac6f
zzammin <woalsdl7399@naver.com>
2023-08-13 16:37:53 +0900

    챌린지를 선택해주세요!

--- app/src/main/java/com/example/swproject/MainActivity.java
+++ app/src/main/java/com/example/swproject/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.swproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.example.swproject.Fragment1;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(5);
+
+        ChallengePagerAdapter adapter = new ChallengePagerAdapter(getSupportFragmentManager());
+        Fragment1 fragment1 = new Fragment1();
+        adapter.addItem(fragment1);
+
+        pager.setAdapter(adapter);
+    }
+
+    class ChallengePagerAdapter extends FragmentStatePagerAdapter{
+        ArrayList<Fragment> items = new ArrayList<Fragment>();
+        public ChallengePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addItem(Fragment item){
+            items.add(item);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
     }
 }
