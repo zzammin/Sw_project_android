a0c9dfe33440d46cc57ccf1f7c8c1c61836358fa
zzammin <woalsdl7399@naver.com>
2023-08-16 18:30:52 +0900

    dark 모드로 시작하던 것 light 모드로 시작하게끔 바꿈

--- app/src/main/java/com/example/swproject/Home.java
+++ app/src/main/java/com/example/swproject/Home.java
@@ -8,11 +8,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class Home extends Fragment {
+    ViewPager2 viewpager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // fragment는 자체 activity가 없기 때문에 (onCreate에 있는) setContentView를 못쓰고, 대신 onCreateView를 사용해야 한다.
+        // viewpager.findViewById()
+
         return inflater.inflate(R.layout.home,container,false);
     }
 }
