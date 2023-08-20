a998caeea346c742165818ff9be431c1e9238833
J1miin <gift040404@naver.com>
2023-08-21 00:58:03 +0900

    08/20 진행중인 챌린지 페이지 뒤로가기 버튼 기능 구현

--- app/src/main/java/com/example/swproject/MypageCompletedChal.java
+++ app/src/main/java/com/example/swproject/MypageCompletedChal.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +15,14 @@ public class MypageCompletedChal extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.mypage_completed_challenge_list, container,false);
+        Button backToMypageBtn = rootView.findViewById(R.id.BackToMypageBtn);
+        backToMypageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyPage myPage = new MyPage();
+                requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fregment_linear, myPage).commit();
+            }
+        });
         return rootView;
     }
 }
