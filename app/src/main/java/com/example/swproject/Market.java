33aa443bc18078aef8089dd34befc2b1f27b80cd
J1miin <gift040404@naver.com>
2023-08-21 04:37:07 +0900

    08/20 상점뷰 인기상품 뷰페이저

--- app/src/main/java/com/example/swproject/Market.java
+++ app/src/main/java/com/example/swproject/Market.java
@@ -19,8 +19,10 @@ public class Market extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.market,container,false);
-        ViewPager pager = (ViewPager)rootView.findViewById(R.id.Today_Sale_pager);
-        pager.setOffscreenPageLimit(3);
+
+        //오늘의 할인 부분 뷰페이저
+        ViewPager MarketSalePager = (ViewPager)rootView.findViewById(R.id.Today_Sale_pager);
+        MarketSalePager.setOffscreenPageLimit(3);
         TodaySaleViewPagerAdapter adapter = new TodaySaleViewPagerAdapter(requireActivity().getSupportFragmentManager());
 
         Market_Sale_Fragment1 fragment1 = new Market_Sale_Fragment1();
@@ -32,7 +34,20 @@ public class Market extends Fragment {
         Market_Sale_Fragment3 fragment3 = new Market_Sale_Fragment3();
         adapter.addItem(fragment3);
 
-        pager.setAdapter(adapter);
+        MarketSalePager.setAdapter(adapter);
+
+        //인기 상품 부분 뷰페이저
+        ViewPager MarketBestItemPager = (ViewPager)rootView.findViewById(R.id.Market_BestItem_pager);
+        MarketBestItemPager.setOffscreenPageLimit(2);
+        BestItemViewPagerAdapter adapter2 = new BestItemViewPagerAdapter(requireActivity().getSupportFragmentManager());
+
+        Market_BestItem_Fragment1 B_I_fragment1 = new Market_BestItem_Fragment1();
+        adapter2.addItem(B_I_fragment1);
+
+        Market_BestItem_Fragment2 B_I_fragment2 = new Market_BestItem_Fragment2();
+        adapter2.addItem(B_I_fragment2);
+
+        MarketBestItemPager.setAdapter(adapter2);
         return rootView;
     }
 
@@ -40,6 +55,27 @@ public class Market extends Fragment {
         ArrayList<Fragment> items = new ArrayList<>();
 
         public TodaySaleViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addItem(Fragment item){
+            items.add(item);
+        }
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+    }
+    class BestItemViewPagerAdapter extends FragmentStatePagerAdapter{
+        ArrayList<Fragment> items = new ArrayList<>();
+
+        public BestItemViewPagerAdapter(@NonNull FragmentManager fm) {
             super(fm);
         }
 
