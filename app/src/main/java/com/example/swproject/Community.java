af3efa1fbbd1eed407a746f65f734d7062adeeb2
J1miin <gift040404@naver.com>
2023-08-21 12:06:39 +0900

    8/21/커뮤니티 틀 + 스터디관리 뷰페이저 연결

--- app/src/main/java/com/example/swproject/Community.java
+++ app/src/main/java/com/example/swproject/Community.java
@@ -8,11 +8,53 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
 
 public class Community extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.community,container,false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.community,container,false);
+
+        ViewPager CommunityStudyPager = (ViewPager)rootView.findViewById(R.id.study_pager);
+        CommunityStudyPager.setOffscreenPageLimit(3);
+        StudyViewPagerAdapter adapter = new StudyViewPagerAdapter(requireActivity().getSupportFragmentManager());
+
+        Community_Study_Fragment1 fragment1 = new Community_Study_Fragment1();
+        adapter.addItem(fragment1);
+
+        Community_Study_Fragment2 fragment2 = new Community_Study_Fragment2();
+        adapter.addItem(fragment2);
+
+        Community_Study_Fragment3 fragment3 = new Community_Study_Fragment3();
+        adapter.addItem(fragment3);
+
+        CommunityStudyPager.setAdapter(adapter);
+        return rootView;
+    }
+
+    class StudyViewPagerAdapter extends FragmentStatePagerAdapter{
+        ArrayList<Fragment> items = new ArrayList<>();
+        public StudyViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+        public void addItem(Fragment item){
+            items.add(item);
+        }
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount()  {
+            return items.size();
+        }
     }
 }
+
