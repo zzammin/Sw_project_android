383221c7f5976bcee1a67efb0b101d2725086553
J1miin <gift040404@naver.com>
2023-08-22 15:15:37 +0900

    8/22/ 커뮤니티 게시판 프레그먼트 연결

--- app/src/main/java/com/example/swproject/Community.java
+++ app/src/main/java/com/example/swproject/Community.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,7 +20,7 @@ public class Community extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.community,container,false);
-
+        //뷰페이저
         ViewPager CommunityStudyPager = (ViewPager)rootView.findViewById(R.id.study_pager);
         CommunityStudyPager.setOffscreenPageLimit(3);
         StudyViewPagerAdapter adapter = new StudyViewPagerAdapter(requireActivity().getSupportFragmentManager());
@@ -34,6 +35,54 @@ public class Community extends Fragment {
         adapter.addItem(fragment3);
 
         CommunityStudyPager.setAdapter(adapter);
+
+        //게시판 button 이벤트
+        //자유게시판 버튼
+        Button freeBoardBtn = (Button)rootView.findViewById(R.id.freeBoardBtn);
+        freeBoardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Community_Board_Free cBoardFreeFragment1 = new Community_Board_Free();
+                requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fregment_linear, cBoardFreeFragment1).commit();
+            }
+        });
+        // 모집 게시판 버튼
+        Button collectBtn = (Button)rootView.findViewById(R.id.collectBoardBtn);
+        collectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Community_Board_Collect cBoardCollectFragment = new Community_Board_Collect();
+                requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fregment_linear, cBoardCollectFragment).commit();
+            }
+        });
+        //정보 게시판 버튼
+        Button infoBtn = (Button)rootView.findViewById(R.id.infoBoardBtn);
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Community_Board_Info cBoardInfoFragment = new Community_Board_Info();
+                requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fregment_linear, cBoardInfoFragment).commit();
+            }
+        });
+        //공모전 게시판 버튼
+        Button contestBtn = (Button)rootView.findViewById(R.id.contestBoardBtn);
+        contestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Community_Board_Contest cBoardContestFragment = new Community_Board_Contest();
+                requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fregment_linear, cBoardContestFragment).commit();
+            }
+        });
+        //hot 게시판 버튼
+        Button hotBtn = (Button)rootView.findViewById(R.id.hotBoardBtn);
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Community_Board_Hot cBoardhotFragment = new Community_Board_Hot();
+                requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fregment_linear, cBoardhotFragment).commit();
+            }
+        });
+
         return rootView;
     }
 
