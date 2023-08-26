40a1b04114aec0421d890ff8c91e4c790ea1a056
J1miin <gift040404@naver.com>
2023-08-26 11:12:08 +0900

    08/26/ 로그인액티비티 - 메인 액티비티로 전환

--- app/src/main/java/com/example/swproject/LoginActivity.java
+++ app/src/main/java/com/example/swproject/LoginActivity.java
@@ -2,7 +2,12 @@ package com.example.swproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText addId= (EditText) findViewById(R.id.addID);
+        EditText addPw= (EditText) findViewById(R.id.addPW);
+        Button loginButton = (Button)findViewById(R.id.loginBtn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userID = addId.getText().toString();
+                String userPW = addPw.getText().toString();
+                if (userID.equals("")||userPW.equals("")){
+                    Toast.makeText(getApplicationContext(), "아이디와 패스워드를 입력해주세요",Toast.LENGTH_LONG).show();
+                }else {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
