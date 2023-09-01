797cedeac6cfff1976add48aeebb8858ed1055fd
PokingTeemo <110599863+PokingTeemo@users.noreply.github.com>
2023-09-01 15:16:13 +0900

    데베 수정

--- app/src/main/java/com/example/swproject/Register.java
+++ app/src/main/java/com/example/swproject/Register.java
@@ -29,7 +29,7 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         // 아이디 찾기
         EditText addID = (EditText)findViewById(R.id.addID);
-        EditText addPW = (EditText)findViewById(R.id.addPW2);
+        EditText addPW2 = (EditText)findViewById(R.id.addPW2);
         EditText addAge = (EditText)findViewById(R.id.addAge);
         EditText addName = (EditText)findViewById(R.id.addName);
         registerBtn = findViewById(R.id.registerBtn);
@@ -39,16 +39,16 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String userID = addID.getText().toString().trim();
-                String userPW = addPW.getText().toString().trim();
+                String userPW2 = addPW2.getText().toString().trim();
                 String checkPW = checkPw.getText().toString();
                 String userName = addName.getText().toString();
                 int userAge = Integer.parseInt(addAge.getText().toString());
 
-                if (userID.isEmpty()||userPW.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "아이디와 패스워드를 입력해주세요",Toast.LENGTH_LONG).show();
-                }else if (!userPW.equals(checkPW)){
-                    Toast.makeText(getApplicationContext(), "비밀번호가 서로 다릅니다", Toast.LENGTH_LONG).show();
-                }else {
+                //if (userID.isEmpty()||userPW2.isEmpty()){
+                    //Toast.makeText(getApplicationContext(), "아이디와 패스워드를 입력해주세요",Toast.LENGTH_LONG).show();
+                //}else if (!userPW2.equals(checkPW)){
+                    //Toast.makeText(getApplicationContext(), "비밀번호가 서로 다릅니다", Toast.LENGTH_LONG).show();
+                //}else{
 
 
                     Response.Listener<String> responseListener = new Response.Listener<String>() {
@@ -56,6 +56,8 @@ public class Register extends AppCompatActivity {
                         public void onResponse(String response) {
                             try {
                                 JSONObject jsonObject = new JSONObject(response);
+
+
                                 boolean success = jsonObject.getBoolean("success");
                                 if(success){ // 회원 가입 성공
                                     Toast.makeText(getApplicationContext(), "회원가입에 성공했습니다. 로그인 페이지로 이동합니다.", Toast.LENGTH_LONG).show();
@@ -74,10 +76,10 @@ public class Register extends AppCompatActivity {
                         }
                     };
 
-                    RegisterRequest registerRequest = new RegisterRequest(userID,userName,userPW,userAge,responseListener);
+                    RegisterRequest registerRequest = new RegisterRequest(userID,userName,userPW2,userAge,responseListener);
                     RequestQueue queue = Volley.newRequestQueue(Register.this);
                     queue.add(registerRequest);
-                }
+                //}
             }
         });
 
