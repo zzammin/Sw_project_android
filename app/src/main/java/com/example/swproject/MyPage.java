cf7989d09d173ff99d0a4cda82f0f1038258b639
PokingTeemo <110599863+PokingTeemo@users.noreply.github.com>
2023-09-06 00:29:27 +0900

    에러코드 수정

--- app/src/main/java/com/example/swproject/MyPage.java
+++ app/src/main/java/com/example/swproject/MyPage.java
@@ -13,7 +13,7 @@ import androidx.fragment.app.Fragment;
 
 public class MyPage extends Fragment {
 
-    Intent intent = getIntent();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
